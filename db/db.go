0cd12a8584ce8e1ee855c162d044b64bedea0056
Francois <54983471+thefran23@users.noreply.github.com>
2024-02-15 10:35:18 +0200

    wip

--- db/db.go
+++ db/db.go
@@ -2,12 +2,42 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 var DB *sql.DB
 
-func InitDb() {
-	DB, err := sql.Open("sqlite3", "api.db")
+func InitDB() {
+
+	db, err := sql.Open("sqlite", "api.sql")
+	if err != nil {
+		panic("Database could not connect: " + err.Error())
+	}
+
+	DB = db
+
+	err = createTables()
+	if err != nil {
+		panic("Database could not connect: " + err.Error())
+	}
+
+	fmt.Println("Tables created successfully!")
+}
+
+func createTables() error {
+	createEventsTable := `
+        CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            location TEXT NOT NULL,
+            dateTime DATETIME NOT NULL,
+            user_id INTEGER
+        )
+    `
+
+	_, err := DB.Exec(createEventsTable)
+	return err
 }
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/db"
 	"example.com/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 	server.GET("/events", getEvents)
 	server.POST("/events", createEvent)
