a389169eee7064e9303148025007ba79b974d8c2
Francois <francois.rossouw@bmwithub.co.za>
2024-02-29 14:47:35 +0200

    wip

---
+++ routes/register.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"example.com/models"
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event"})
+		return
+	}
+
+}
+
+func cancelRegistration(context *gin.Context) {}
